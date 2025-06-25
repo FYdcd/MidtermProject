62d4b2ce885a15ed52bbb9a5561a06caf898f50e
241005 <241005@std.hi-joho.ac.jp>
2025-06-25 09:36:59 +0900

    イェイ

--- src/Slime.java
+++ src/Slime.java
@@ -3,7 +3,6 @@ public final class Slime extends Character{
         super(name,hp);
     }
 
-    @Override
     public void attack(Character target) {
         System.out.println(this.name+"は体当たり攻撃！"+target.name+"に5のダメージを与えた！");
         target.hp -= 5;
